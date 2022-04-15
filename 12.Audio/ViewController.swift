b84e3ef324c98ff258b917154fe6a3d4df74f90b
ByeongHyeon <qudgus1984@naver.com>
2022-04-15 17:31:30 +0900

    outlet

--- 12.Audio/ViewController.swift
+++ 12.Audio/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var pvProgressPlay: UIProgressView!
+    @IBOutlet var lblCurrentTime: UILabel!
+    @IBOutlet var lblEndTime: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
